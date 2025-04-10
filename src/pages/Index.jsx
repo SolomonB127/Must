1ecdd017e8e3a14bc98fb227244610421afc68b5
Golden Tech <techbygolden@gmail.com>
2025-04-10 16:43:33 +0100

    feat: Add Index page with NavBar component
    - Created the Index page and included the NavBar component inside a <main> element.

---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const Index = () => {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+
+export default Index
