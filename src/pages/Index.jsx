280ba0645efe147a7843deac790c7020130c8475
Golden Tech <techbygolden@gmail.com>
2025-04-11 14:32:06 +0100

    feat: Add Hero component to Index page
    - Integrated the Hero component into the Index page.
    - Positioned the Hero section below the NavBar.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
 
 const Index = () => {
     return (
         <main>
             <NavBar />
+            <Hero />
         </main>
     )
 }
